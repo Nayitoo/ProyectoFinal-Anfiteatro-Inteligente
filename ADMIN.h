d74006c53661afd20b4d7aac21689580afe3b9ee
Nayitoo <ignacio.cerredo@gmail.com>
2025-08-26 09:11:04 -0300

    Add files via upload

---
+++ ADMIN.h
@@ -0,0 +1,14 @@
+const char ADMIN[] = 
+R"=====( 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Web000</title>
+    <link rel="icon" href="data:,">
+</head>
+<body>
+    <h1>Estado del pulsador</h1>
+    <p>Pulsador en: VALOR ACTUAL PULSADOR</p>
+</body>
+</html>
+)=====";
